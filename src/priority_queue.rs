e1e0d0a9ede75c3d8c960323508140a0af31b18d
Johannes Hengstler <ubezl@student.kit.edu>
2023-03-08 21:33:30 +0100

    implemented select_k

--- src/priority_queue.rs
+++ src/priority_queue.rs
@@ -1,12 +1,27 @@
-use crate::p_sample_sort;
+use std::cmp::{max, min};
 use crate::util::select_sample;
 
 const LOCAL_SORT_THRESHOLD: usize = 512;
 
 /// Las-Vegas-algorithm to select the k smallest elements in a distributed set. Chernoff bound guarantees
 /// constant number of recursions with high probability. Algorithm by Blum et al. 1972.
-fn select_k(data: &mut [u64], k: usize) -> Vec<u64> {
+/// Guarantees that all data is still present in input array, but may be sorted.
+///
+/// # Parameters
+/// - `data` a mutable slice of data. May can sorted in-place, hence the mutability
+/// - `k` amount of elements to select from `data`. Must not be greater than length of `data`.
+///
+/// # Returns
+/// A vector of the k smallest elements of the input data slice. Not sorted.
+pub fn select_k(data: &mut [u64], k: usize) -> Vec<u64> {
+    assert!(data.len() >= k, "cannot select more elements than present");
+
     let delta = 16; // tuning parameter
+
+    // sanity optimization
+    if data.len() == k {
+        return data.to_vec();
+    }
 
     if data.len() < LOCAL_SORT_THRESHOLD {
         data.sort_unstable();
@@ -18,21 +33,71 @@ fn select_k(data: &mut [u64], k: usize) -> Vec<u64> {
     sample.sort_unstable();
 
     let ratio = k as f64 / data.len() as f64;
-    let upper = sample[ratio * LOCAL_SORT_THRESHOLD as usize + delta];
-    let lower = sample[ratio * LOCAL_SORT_THRESHOLD as usize - delta];
+    let bound = (ratio * LOCAL_SORT_THRESHOLD as f64) as usize;
+
+    let upper = sample[min(bound + delta, sample.len() - 1)];
+    let lower = sample[max(bound as isize - delta as isize, 0) as usize];
 
     let mut lower_bucket = Vec::with_capacity(k + delta);
     let mut middle_bucket = Vec::with_capacity(2 * delta);
     let mut upper_bucket = Vec::with_capacity(data.len() - k - delta);
 
     for n in data {
-        if n < lower {
+        if *n < lower {
             lower_bucket.push(*n);
-        } else if n > upper {
+        } else if *n > upper {
             upper_bucket.push(*n);
         } else {
             middle_bucket.push(*n);
         }
     }
-    todo!()
+
+    if lower_bucket.len() > k {
+        select_k(&mut lower_bucket, k)
+    } else {
+        if lower_bucket.len() + middle_bucket.len() < k {
+            lower_bucket.append(&mut middle_bucket);
+            lower_bucket.append(&mut select_k(&mut upper_bucket, k - lower_bucket.len()));
+            lower_bucket
+        } else {
+            lower_bucket.append(&mut select_k(&mut middle_bucket, k - lower_bucket.len()));
+            lower_bucket
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::distributions::Uniform;
+    use rand::{Rng, thread_rng};
+    use super::LOCAL_SORT_THRESHOLD;
+    use super::select_k;
+
+    #[test]
+    fn test_select_k() {
+        let select_single = select_k(&mut [1], 1);
+        assert_eq!(1, select_single.len());
+        assert_eq!(1, select_single[0]);
+
+        let select_multiple = select_k(&mut [1, 10, 3, 5, 6, 1, 2], 2);
+        assert_eq!(2, select_multiple.len());
+        assert_eq!(1, select_multiple[0]);
+        assert_eq!(1, select_multiple[1]);
+
+        let select_none = select_k(&mut [], 0);
+        assert_eq!(0, select_none.len());
+
+        let mut rng = thread_rng();
+        let uniform = Uniform::from(10..100);
+        let mut data = Vec::with_capacity(2 * LOCAL_SORT_THRESHOLD);
+        for _ in 0..2 * LOCAL_SORT_THRESHOLD {
+            data.push(rng.sample(&uniform));
+        }
+        data.push(1);
+        data.push(2);
+        data.push(3);
+        let smallest = select_k(&mut data, 3);
+        assert_eq!(3, smallest.len());
+        assert!(smallest.contains(&1) && smallest.contains(&2) && smallest.contains(&3))
+    }
 }
