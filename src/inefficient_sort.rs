f566bf2ca1d8dd700547796476d310afc0e2458c
Johannes Hengstler <ubezl@student.kit.edu>
2023-03-08 23:06:00 +0100

    implemented universal inefficient_sort

--- src/inefficient_sort.rs
+++ src/inefficient_sort.rs
@@ -13,14 +13,32 @@ use mpi::traits::{Communicator, Root};
 pub fn p_inefficient_sort(comm: &SystemCommunicator, data: &mut [u64]) {
     let rank = comm.rank() as usize;
     let world_size = comm.size() as usize;
-    let mut recv_buffer = vec![0, data.len() * world_size];
+    let mut recv_buffer = vec![0u64; data.len() * world_size];
+    let root = comm.process_at_rank(0);
 
     if rank == 0 {
-        comm.this_process().gather_into_root(data, &mut recv_buffer);
+        root.gather_into_root(data, &mut recv_buffer);
         recv_buffer.sort_unstable();
-        comm.this_process().scatter_into_root(&recv_buffer, data);
+        root.scatter_into_root(&recv_buffer, data);
     } else {
-        comm.process_at_rank(0i32).gather_into(data);
-        comm.process_at_rank(0i32).scatter_into(data);
+        root.gather_into(data);
+        root.scatter_into(data);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::p_inefficient_sort;
+
+    #[test]
+    fn test_inefficient_sort() {
+        let mut data = [234, 23, 4, 234, 23, 4, 234, 23, 2, 1362, 6, 1, 36, 7];
+        let universe = mpi::initialize().unwrap();
+        let world = universe.world();
+
+        p_inefficient_sort(&world, &mut data);
+        let expected = [1, 2, 4, 4, 6, 7, 23, 23, 23, 36, 234, 234, 234, 1362u64];
+        assert_eq!(expected.len(), data.len());
+        expected.iter().zip(data.iter()).for_each(|(i, j)| assert_eq!(*i, *j));
     }
 }
