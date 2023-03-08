02b54cd1027ee8294d1c2d830026dfb211fbcdc0
Johannes Hengstler <ubezl@student.kit.edu>
2023-03-08 20:07:37 +0100

    timing tests now take an optional argument for the data sample size, and both handle the argument equally with respect to total data size across all clients

--- examples/local_sort_timing_test.rs
+++ examples/local_sort_timing_test.rs
@@ -1,17 +1,34 @@
+use std::env;
+use std::process::exit;
 use std::time::Instant;
 
 use rand::{Rng, thread_rng};
 use rand::distributions::Uniform;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let initial_data_amount = if args.len() > 2 {
+        eprintln!("usage: {} [sample-size]", args[0]);
+        exit(1);
+    } else if args.len() == 2 {
+        if args[1].eq_ignore_ascii_case("--help") {
+            println!("usage: {} [sample-size]", args[0]);
+            exit(0);
+        }
+
+        usize::from_str_radix(&args[1], 10).expect("first argument should be an integer")
+    } else {
+        1 << 26
+    };
+
     // generate random data
     let mut rng = thread_rng();
     let uniform = Uniform::from(0..u64::MAX);
-    let initial_data_amount = 1 << 26;
 
-    let mut data = vec![0u64; initial_data_amount];
-    for i in 0..data.len() {
-        data[i] = rng.sample(&uniform);
+
+    let mut data = Vec::with_capacity(initial_data_amount);
+    for _ in 0..initial_data_amount {
+        data.push(rng.sample(&uniform));
     }
 
     // call sorting algorithm
--- examples/sample_sort_integration_test.rs
+++ examples/sample_sort_integration_test.rs
@@ -1,3 +1,5 @@
+use std::env;
+use std::process::exit;
 use mpi::traits::{Communicator, Destination, Source};
 use rand::distributions::Uniform;
 use rand::{thread_rng, Rng};
@@ -6,15 +8,20 @@ use std::time::Instant;
 use parallel_toolbox::p_sample_sort;
 
 fn main() {
-    // generate random data
-    let mut rng = thread_rng();
-    let uniform = Uniform::from(0..u64::MAX);
-    let initial_data_amount = 1 << 24;
+    let args: Vec<String> = env::args().collect();
+    let initial_data_amount = if args.len() > 2 {
+        eprintln!("usage: {} [sample-size]", args[0]);
+        exit(1);
+    } else if args.len() == 2 {
+        if args[1].eq_ignore_ascii_case("--help") {
+            println!("usage: {} [sample-size]", args[0]);
+            exit(0);
+        }
 
-    let mut data = vec![0u64; initial_data_amount];
-    for i in 0..data.len() {
-        data[i] = rng.sample(&uniform);
-    }
+        usize::from_str_radix(&args[1], 10).expect("first argument should be an integer")
+    } else {
+        1 << 26
+    };
 
     // initialize MPI
     let universe = mpi::initialize().unwrap();
@@ -22,21 +29,32 @@ fn main() {
     let rank = world.rank() as usize;
     let world_size = world.size() as usize;
 
+    // generate random data
+    let mut rng = thread_rng();
+    let uniform = Uniform::from(0..u64::MAX);
+
+    let mut data = Vec::with_capacity(initial_data_amount / world_size);
+    for _ in 0..initial_data_amount / world_size {
+        data.push(rng.sample(&uniform));
+    }
+
     // call sorting algorithm
     let start = Instant::now();
-    let data = p_sample_sort(&world, &data, data.len() * world_size);
+    let data = p_sample_sort(&world, &data, initial_data_amount);
     println!(
         "process {} sorted {} of {} qwords in {} ms",
         rank,
         data.len(),
-        world_size * initial_data_amount,
+        initial_data_amount,
         start.elapsed().as_millis()
     );
 
+    // check data is sorted locally
     for i in 0..data.len() - 1 {
         assert!(data[i] <= data[i + 1]);
     }
 
+    // check data is sorted across PUs
     let mut next_elem = 0u64;
     mpi::request::scope(|scope| {
         let req = if rank > 0 {
